c4df1f19527ba809c7ee11951b1158417bdafaab
sarveshhome <sarvesh.carlton@gmail.com>
2021-08-01 10:04:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-bvhfzz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-bvhfzz)
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <>
+      <h1>Hello Redux !</h1>
+      <p>Redux is a state management system. Therefore, we will need:</p>
+      <ul>
+        <li> a place to save the state </li>
+        <li> a method to get the state </li>
+        <li> a method to change the state</li>
+      </ul>
+    </>
+  );
+}
