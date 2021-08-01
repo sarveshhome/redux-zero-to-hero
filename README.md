c7f1028176448a71650f87e0af2a7575f80ec772
sarveshhome <sarvesh.carlton@gmail.com>
2021-08-01 10:21:04 +0100

    store

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # react-bvhfzz
 
+# Redux Data Flow and React Component Life Cycle
+
+Redux store
+
+step 1: 
+
+first create store folder. then create index.js 
+
+1.store is the place we save the state
+
+`import { createStore } from 'redux';
+
+const store = createStore();`
+
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-bvhfzz)
--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ export default function App() {
   return (
     <>
       <h1>Hello Redux !</h1>
-      <p>Redux is a state management system. Therefore, we will need:</p>
+      <p>Redux is a state management system.</p>
       <ul>
         <li> a place to save the state </li>
         <li> a method to get the state </li>
         <li> a method to change the state</li>
       </ul>
+
+      
     </>
   );
 }
