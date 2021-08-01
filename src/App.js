332bf268e295b71d355e26b167e3aa9cfbaa4749
sarveshhome <sarvesh.carlton@gmail.com>
2021-08-01 14:13:00 +0100

    import react in counter file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './style.css';
-import Counter from './components/Counter';
+import { Counter } from './components/Counter.js';
 export default function App() {
   return (
     <>
